39908ae2bff52e2bce440e6593ca898041adbf04
SilverComet7 <841531691@qq.com>
2022-02-25 18:54:18 +0800

    新增proComponent

--- examples/umiApp/.umirc.ts
+++ examples/umiApp/.umirc.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  layout: {},
+  mfsu: {},
+  layout: {
+
+  },
   nodeModulesTransform: {
     type: 'none',
   },
+  history: { type: 'hash' },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      path: '/system', component: '@/layouts', name: '系统管理', routes: [
+        // { path: '/', component: '@/pages/index', name: '角色配置' },
+        { path: '/admins', name: '管理员配置', component: '@/pages/system-setting/admins' }
+      ]
+    }, {
+      path: '/', component: '@/layouts', name: '指令', routes: [{
+        path: '/user-search', component: '@/pages/command/userSearch', name: '用户查询'
+      }]
+    },
+    // { path: '/login', component: '@/pages/login' }
   ],
   fastRefresh: {},
 });
